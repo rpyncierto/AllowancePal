76ae8203de5279f5104d9da7cede8bd3114249c2
Reymond Yncierto <ynciertorp@gmail.com>
2023-06-24 10:38:31 +0800

    chore: added container for the list of accounts

--- lib/screens/tracker/dashboard.dart
+++ lib/screens/tracker/dashboard.dart
@@ -49,8 +49,8 @@ class _DashboardPageState extends State<DashboardPage> {
     }
 
     double spendableAmount = 0.0;
-      for (var account in accounts) {
-        if (account.type == 'Spendable') {
+    for (var account in accounts) {
+      if (account.type == 'Spendable') {
         spendableAmount += (account.balance * 0.7);
       }
     }
@@ -145,38 +145,48 @@ class _DashboardPageState extends State<DashboardPage> {
                 ),
                 const SizedBox(height: 8.0),
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: accounts.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        elevation: 2.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: ListTile(
-                          title: Text(
-                            accounts[index].name,
-                            style: TextStyle(
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(
+                          8.0), // Adjust the padding value as needed
+                      child: ListView.builder(
+                        itemCount: accounts.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
-                          ),
-                          subtitle: Text(
-                            'Balance: \$${accounts[index].balance.toStringAsFixed(2)}',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.grey[600],
+                            child: ListTile(
+                              title: Text(
+                                accounts[index].name,
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: Text(
+                                'Balance: \$${accounts[index].balance.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                              trailing: IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  _deleteAccount(index);
+                                },
+                              ),
                             ),
-                          ),
-                          trailing: IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              _deleteAccount(index);
-                            },
-                          ),
-                        ),
-                      );
-                    },
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -227,96 +237,96 @@ class _DashboardPageState extends State<DashboardPage> {
     });
   }
 
- void _showAddAccountDialog() {
-  // Define variables to hold the selected value
-  String? selectedAccountType;
+  void _showAddAccountDialog() {
+    // Define variables to hold the selected value
+    String? selectedAccountType;
 
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('Add Account'),
-      content: StatefulBuilder(
-        builder: (BuildContext context, StateSetter setState) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Account Name'),
-                onChanged: (value) {
-                  setState(() {
-                    accountName = value;
-                  });
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Balance'),
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  setState(() {
-                    balance = double.parse(value);
-                  });
-                },
-              ),
-              SizedBox(height: 25.0),
-              Text(
-                'Account Type:',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              ListTile(
-                title: Text('Spendable'),
-                leading: Radio<String>(
-                  value: 'Spendable',
-                  groupValue: selectedAccountType,
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add Account'),
+        content: StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(labelText: 'Account Name'),
                   onChanged: (value) {
                     setState(() {
-                      selectedAccountType = value;
+                      accountName = value;
                     });
                   },
                 ),
-              ),
-              ListTile(
-                title: Text('Savings'),
-                leading: Radio<String>(
-                  value: 'Savings',
-                  groupValue: selectedAccountType,
+                TextField(
+                  decoration: InputDecoration(labelText: 'Balance'),
+                  keyboardType: TextInputType.number,
                   onChanged: (value) {
                     setState(() {
-                      selectedAccountType = value;
+                      balance = double.parse(value);
                     });
                   },
                 ),
-              ),
-            ],
-          );
-        },
-      ),
-      actions: [
-        TextButton(
-          child: Text('Cancel'),
-          onPressed: () {
-            Navigator.pop(context);
+                SizedBox(height: 25.0),
+                Text(
+                  'Account Type:',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                ListTile(
+                  title: Text('Spendable'),
+                  leading: Radio<String>(
+                    value: 'Spendable',
+                    groupValue: selectedAccountType,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedAccountType = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: Text('Savings'),
+                  leading: Radio<String>(
+                    value: 'Savings',
+                    groupValue: selectedAccountType,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedAccountType = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            );
           },
         ),
-        TextButton(
-          child: Text('Add'),
-          onPressed: () async {
-            if (selectedAccountType != null) {
-              accounts.add(Account(accountName, balance, selectedAccountType!));
-              await _saveAccountData(); // Save the updated account data
-              _fetchAccountData(); // Fetch updated account data
+        actions: [
+          TextButton(
+            child: Text('Cancel'),
+            onPressed: () {
               Navigator.pop(context);
-            }
-          },
-        ),
-      ],
-    ),
-  );
-}
-
+            },
+          ),
+          TextButton(
+            child: Text('Add'),
+            onPressed: () async {
+              if (selectedAccountType != null) {
+                accounts
+                    .add(Account(accountName, balance, selectedAccountType!));
+                await _saveAccountData(); // Save the updated account data
+                _fetchAccountData(); // Fetch updated account data
+                Navigator.pop(context);
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 class Account {
